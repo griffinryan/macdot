04764d0e517c7a7fb6f0b1fa37a52fc37e66d223
torpoise <torpoise@icloud.com>
2021-01-10 06:32:11 -0800

    fixed dotfiles scripting

--- scripts/dot.sh
+++ scripts/dot.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 # dotfile management script
-NC='\033[0m';               # No Color
-LIGHTBLUE='\033[1;34m';     # Light Blue
-LIGHTYELLOW='\033[1;33m';   # Light Yellow
-LIGHTPURPLE='\033[1;35m';   # Light Purple
-LIGHTRED='\033[1;31m';      # Light Red
+NC='\033[0m'               # No Color
+LIGHTBLUE='\033[1;34m'     # Light Blue
+LIGHTYELLOW='\033[1;33m'   # Light Yellow
+LIGHTPURPLE='\033[1;35m'   # Light Purple
+LIGHTRED='\033[1;31m'      # Light Red
 
 function dotfileInstall() {
-    cp -r /usr/local/share/torpdots/dotfiles/* ~/;
+    cp -r /usr/local/share/torpdots/dotfiles/* ~/
     # copy iTerm2 settings
     cp /usr/local/share/torpdots/dotfiles/.iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 }
 
-echo;
-echo -e "    Now installing all ${LIGHTBLUE}torpdots${NC} dotfiles to home directory...";
+echo
+echo -e "    Now installing all ${LIGHTBLUE}torpdots${NC} dotfiles to home directory..."
 
 # Install dotfiles to ~/.* from /usr/local/share/torpdots/dotfiles/.*
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     # force dotfileInstall() with -f argument
-    dotfileInstall;
-    echo -e "    ${LIGHTBLUE}torpdots${NC} ${YELLOW}dot${NC} has successfully installed all dotfiles!";
+    dotfileInstall
+    echo -e "    ${LIGHTBLUE}torpdots${NC} ${YELLOW}dot${NC} has successfully installed all dotfiles!"
 else
-    read -p "    This will overwrite existing files in the home directory. Are you sure? (y/n) " -n 1;
+    read -p "    This will overwrite existing files in the home directory. Are you sure? (y/n) " -n 1
     echo;
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        dotfileInstall;
-        echo -e "    ${LIGHTBLUE}torpdots${NC} ${YELLOW}dot${NC} has successfully installed all dotfiles!";
+        dotfileInstall
+        echo -e "    ${LIGHTBLUE}torpdots${NC} ${YELLOW}dot${NC} has successfully installed all dotfiles!"
     fi;
 fi;
-unset dotfileInstall;
+unset dotfileInstall
