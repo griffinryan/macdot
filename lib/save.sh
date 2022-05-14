305395d42957e20b300424b9bccf9e36b3275981
griffinryan <glryan@uw.edu>
2022-05-14 14:05:07 -0700

    added backup utility `save`

--- lib/save.sh
+++ lib/save.sh
@@ -17,14 +17,46 @@ LIGHTYELLOW='\033[1;33m';   # Light Yellow
 PURPLE='\033[0;35m';        # Purple
 LIGHTPURPLE='\033[1;35m';   # Light Purple
 
+function dotfileSave() {
+    # make a new /.macrice/ folder.
+    cd $HOME
+    mkdir .macrice 2>/dev/null
+    mkdir .macrice/.vim 2>/dev/null
+    mkdir .macrice/.local 2>/dev/null
+    mkdir .macrice/.config 2>/dev/null
+    mkdir .macrice/.iterm 2>/dev/null
+    mkdir .macrice/.fonts 2>/dev/null
+
+    # individually (and recursively) add parent directories.
+    /bin/cp -r ~/.vim ~/.macrice/.vim 2>/dev/null
+    /bin/cp -r ~/.local ~/.macrice/.local 2>/dev/null
+    /bin/cp -r ~/.config ~/.macrice/.config 2>/dev/null
+    /bin/cp -r ~/.iterm ~/.macrice/.iterm 2>/dev/null
+    /bin/cp -r ~/.fonts ~/.macrice/.fonts 2>/dev/null
+
+    # wildcard * to add all parent files.
+    /bin/cp ~/.* ~/.macrice/ 2>/dev/null
+}
+
 # Help page for macrice - a macOS configuration tool.
-toilet -d /opt/homebrew/Cellar/macrice/**/fonts -f larry3d 'Macrice' | boxes -d parchment | lolcat -p 5
-# Status message.
-echo "       '${LIGHTPURPLE}macrice${NC} ${LIGHTYELLOW}save${NC}' is now backing up all dotfiles in ${LIGHTBLUE}/~/...${NC}";
-
-############
-
-
-# Status message
-echo "       '${LIGHTPURPLE}macrice${NC} ${LIGHTYELLOW}save${NC}' is now...     ${LIGHTBLUE}complete!${NC}";
-echo "       '${LIGHTPURPLE}macrice${NC} ${LIGHTYELLOW}save${NC}' backed up dotfiles to ${LIGHTBLUE}/~/.macrice/${NC}"
+    toilet -d /opt/homebrew/Cellar/macrice/**/fonts -f larry3d 'Macrice' | boxes -d parchment | lolcat -p 5
+# Install dotfiles to ~/.* from /usr/local/share/torpdots/dotfiles/.*
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    # force dotfileInstall() with -f argument
+    dotfileSave
+    # Status message.
+    echo "       '${LIGHTPURPLE}macrice${NC} ${LIGHTYELLOW}save${NC}' backed up dotfiles to ${LIGHTBLUE}/~/.macrice/${NC}"
+else
+    # Status message.
+    echo "    This will ${LIGHTRED}overwrite${NC} the ${LIGHTRED}existing${NC} backup in ${LIGHTBLUE}/~/.macrice/${NC}. ${LIGHTRED}Are you sure?${NC} "
+    read -p "   (y/n):" -n 1
+    echo;
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        dotfileSave
+        # Help page for macrice - a macOS configuration tool.
+        toilet -d /opt/homebrew/Cellar/macrice/**/fonts -f larry3d 'Macrice' | boxes -d parchment | lolcat -p 5
+        # Status message.
+        echo "       '${LIGHTPURPLE}macrice${NC} ${LIGHTYELLOW}save${NC}' backed up dotfiles to ${LIGHTBLUE}/~/.macrice/${NC}"
+    fi;
+fi;
+unset dotfileInstall
